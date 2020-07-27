266c65242d64a89d630758367e864749379454ea
omarion3698 <omaribinbakarivic@gmail.com>
2020-07-27 21:04:35 +0300

    Last commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,96 +1,97 @@
-$(document).ready(function(){
-  $("#development-image").click(function(){
+$(document).ready(function() {
+  $("#development-image").click(function() {
     $("#development-image").slideDown('1700').hide('1200');
     $("#development").show('1700');
   });
-  $("#development").click(function(){
+  $("#development").click(function() {
     $("#development").slideUp('1700');
     $("#development-image").slideDown('1700');
   });
 });
 
-$(document).ready(function(){
-  $("#design-image").click(function(){
+$(document).ready(function() {
+  $("#design-image").click(function() {
     $("#design-image").slideDown('1700').hide('1200');
     $("#design").show('1700');
   });
-  $("#design").click(function(){
+  $("#design").click(function() {
     $("#design").slideUp('1700');
     $("#design-image").slideDown('1700');
   });
 });
 
-$(document).ready(function(){
-  $("#product-image").click(function(){
+$(document).ready(function() {
+  $("#product-image").click(function() {
     $("#product-image").slideDown('1700').hide('1200');
     $("#product").show('1700');
   });
-  $("#product").click(function(){
+  $("#product").click(function() {
     $("#product").slideUp('1700');
     $("#product-image").slideDown('1700');
   });
 });
 
-$(document).ready(function(){
-  $("#work1").mouseover(function(){
+$(document).ready(function() {
+  $("#work1").mouseover(function() {
     $("#overlay").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay").hide();
   });
 });
-$(document).ready(function(){
-  $("#work2").mouseover(function(){
+
+$(document).ready(function() {
+  $("#work2").mouseover(function() {
     $("#overlay2").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay2").hide();
   });
 });
-$(document).ready(function(){
-  $("#work3").mouseover(function(){
+
+$(document).ready(function() {
+  $("#work3").mouseover(function() {
     $("#overlay3").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay3").hide();
   });
 });
-$(document).ready(function(){
-  $("#work4").mouseover(function(){
+
+$(document).ready(function() {
+  $("#work4").mouseover(function() {
     $("#overlay4").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay4").hide();
   });
 });
 
-$(document).ready(function(){
-  $("#work5").mouseover(function(){
+$(document).ready(function() {
+  $("#work5").mouseover(function() {
     $("#overlay5").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay5").hide();
   });
-  $("#work6").mouseover(function(){
+  $("#work6").mouseover(function() {
     $("#overlay6").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay6").hide();
   });
-  $("#work7").mouseover(function(){
+  $("#work7").mouseover(function() {
     $("#overlay7").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay7").hide();
   });
-  $("#work8").mouseover(function(){
+  $("#work8").mouseover(function() {
     $("#overlay8").show();
-  }).mouseout(function(){
+  }).mouseout(function() {
     $("#overlay8").hide();
   });
 });
 
-//UI logic//
-
-$(document).ready(function(){
-  $("form#form34A").submit(function(event){
+$(document).ready(function() {
+  $("form#form34A").submit(function(event) {
     var name = $("input#MERGE1").val();
     var email = $("input#MERGE0").val();
     var message = $("textarea#comment").val();
-    if ($("input#MERGE1").val() && $("input#MERGE0").val()){
+    if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
       alert (name + ", we have received your message. Thank you for contacting us.");
     }
     else {
